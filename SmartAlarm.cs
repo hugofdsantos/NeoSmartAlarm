a03ab60d2be6be9d35d937b37c099e0c665eec7a
Hugo Fernandes <hugo.fernandess01@gmail.com>
2019-05-19 15:19:59 -0300

    Introducing external method

--- SmartAlarm.cs
+++ SmartAlarm.cs
@@ -4,33 +4,40 @@ using Neo.SmartContract.Framework.Services.Neo;
 
 namespace Neo.SmartContract
 {
+    public static class HelperExternal
+    {
+        [Syscall("Neo.Blockchain.GetContract")]
+        public static extern bool GetContract(byte[] scriptHash);
+    }
     public class SmartAlarm : Framework.SmartContract
     {
         public delegate bool AnotherContract(string method, object[] args);
         private static readonly byte[] owner = "AcjwdaeMyuzxRUJd4LdqHNML627hKZpdhq".ToScriptHash();
-        public static bool Main(string operation, params object[] args)
+        private static readonly byte[] test = "7b3984fec46a1410cccc56da5d52536fd1bf2330".HexToBytes();
+
+        public static bool Main(string operation, object[] args, params object[] paramsToInvoke)
         {
             /*
             integers are always multiplied by 10^3 to simulate float numbers
-            args[0] => script hash of the contract to be registered (scheduled)
+            args[0] => script hash of the contract to be registered reversed (scheduled)
             args[1] => how frequent the contract should run
-            args[2] => the owner wants the contract to run with priority? if yes, the parameter receives GAS currency
              */
+
             if (Runtime.Trigger == TriggerType.Verification)
             {
                 return Runtime.CheckWitness(owner);
             }
             else if (Runtime.Trigger == TriggerType.Application)
             {
-                if (operation == "register") return Register(args);
-                else if (operation == "execute") return Execute(args);
-                else if (operation == "cancel") return Cancel(args);
+                if (operation == "register") return Register(args, paramsToInvoke);
+                else if (operation == "execute") return Execute(args, paramsToInvoke);
             }
             Runtime.Notify("No operation found!");
+
             return false;
         }
 
-        static bool Execute(object[] args)
+        static bool Execute(object[] args, object[] paramsToInvoke)
         {
             var contract = Blockchain.GetContract((byte[])args[0]);
             var serializedEntry = Storage.Get(Storage.CurrentContext, contract.Script);
@@ -46,7 +53,7 @@ namespace Neo.SmartContract
             var timeStamp = Blockchain.GetHeader(Blockchain.GetHeight()).Timestamp;
             if (timeStamp - lastExecution <= frequency)
             {
-                Runtime.Notify("Its not time for contract execution yet.");
+                Runtime.Notify("Its not time for contract execution.");
                 return false;
             }
 
@@ -54,10 +61,10 @@ namespace Neo.SmartContract
             var contractParams = (object[])entry[0];
             var contractToBeInvoked = (AnotherContract)contract.Script.ToDelegate();
             var result = (bool)contractToBeInvoked((string)contractParams[0], (object[])contractParams[1]);
-            if(!result)
-            {  
-               Runtime.Notify("Error in contract execution.");
-               return false; 
+            if (!result)
+            {
+                Runtime.Notify("Error in contract execution.");
+                return false;
             }
 
             entry[2] = timeStamp;
@@ -65,17 +72,19 @@ namespace Neo.SmartContract
             return true;
         }
 
-        static bool Register(object[] args)
+        static bool Register(object[] args, object[] paramsToInvoke)
         {
             object[] newEntry = new object[3];
 
-            var contract = Blockchain.GetContract((byte[])args[0]);
-            if (contract.Script == null)
+            // var scriptHashReversed = (byte[])args[0];        
+            var scriptHashReversed = test;
+            var contract = HelperExternal.GetContract(scriptHashReversed);
+            if (!contract)
             {
                 Runtime.Notify("The given scripthash is invalid.");
                 return false;
             }
-            else if (Storage.Get(Storage.CurrentContext, contract.Script) != null)
+            else if ((Storage.Get(Storage.CurrentContext, scriptHashReversed)).Length != 0)
             {
                 Runtime.Notify("Contract already registered.");
                 return false;
@@ -88,18 +97,13 @@ namespace Neo.SmartContract
                 return false;
             }
 
-            var contractParams = (object[])args[3];
-
-            newEntry[0] = contractParams;
+            // var contractParams = paramsToInvoke;
+            newEntry[0] = paramsToInvoke;
             newEntry[1] = frequency;
             newEntry[2] = 0; //last execution
-
-            Storage.Put(Storage.CurrentContext, contract.Script, newEntry.Serialize());
+            Storage.Put(Storage.CurrentContext, scriptHashReversed, newEntry.Serialize());
+            
             return true;
-        }
-        static bool Cancel(object[] args)
-        {
-            return false;
         }
     }
 }
